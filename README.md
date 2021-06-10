31e31d74d83be5cfd3f6506820df6ebdc3466e4a
Harshit <seth_harshit3182008@outlook.com>
2021-06-10 17:47:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+
+# Project Title
+
+A brief description of what this project does and who it's for
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/HarshitSeth-809014/ChatApp_NodeJS.git
+```
+
+Go to the project directory
+
+```bash
+  cd my_project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  node server/index.js
+```
+
+  
+## Screenshots
+
+![App Screenshot](/Screenshots/Screenshot.png)
+
+  
+## Tech Stack
+
+**Client:** HTML, CSS and JavaScript
+
+**Server:** NodeJS, SocketIO
+
+  
+## Authors
+Name: Harshit Seth\
+Class: 8\
+Age: 13 years
+- [@HarshitSeth-809014](https://github.com/HarshitSeth-809014/)
+
+  
---
+++ js/client.js
@@ -0,0 +1,64 @@
+const socket = io('http://localhost:8000')
+
+const ting = new Audio('../ting.mp3')
+const join = new Audio('../join.mp3')
+const leave = new Audio('../leave.mp3')
+
+const form = document.getElementById('send-con')
+const message = document.getElementById('text')
+const messageContainer = document.querySelector('.container')
+
+const appendJoin = (message)=>{
+    const messEle = document.createElement('div')
+    messEle.classList.add('joinLeave')
+    messEle.innerHTML = `<span><b>${message}</b> joined the chat</span>`
+    messageContainer.append(messEle)
+}
+const appendLeave = (message)=>{
+    const messEle = document.createElement('div')
+    messEle.classList.add('joinLeave')
+    messEle.innerHTML = `<span><b>${message}</b> left the chat</span>`
+    messageContainer.append(messEle)
+}
+const appendMess = (name, mess, pos)=>{
+    const messEle = document.createElement('div')
+    messEle.classList.add('messages')
+    messEle.classList.add(pos)
+
+    const messMem = document.createElement('div')
+    messMem.classList.add('member')
+    messMem.innerText = name
+    
+    const messMes = document.createElement('div')
+    messMes.classList.add('message')
+    messMes.innerText = mess
+
+    messEle.append(messMem)
+    messEle.append(messMes)
+
+    messageContainer.append(messEle)
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const mess = message.value
+    message.value = ''
+    appendMess("You", mess, 'right')
+    socket.emit('send', mess)
+})
+
+const nameK = prompt('Enter your name to join: ')
+socket.emit('new-user-joined', nameK)
+
+socket.on('user-joined', data => {
+    appendJoin(data)
+    join.play()
+})
+socket.on('receive', data => {
+    appendMess(data.name, data.message, 'left')
+    ting.play()
+})
+socket.on('left', data => {
+    appendLeave(data)
+    leave.play()
+})
